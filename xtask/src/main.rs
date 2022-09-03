676c7a4af7dc094fa13a1bc33ce0d473eef5688f
Quentin Boyer <quentin.boyer@atos.net>
2022-09-03 12:22:13 +0200

    Basic xtask framework

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{CommandFactory, Parser};
+
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    task: Option<Task>,
+}
+
+#[derive(Parser)]
+enum Task {
+    Build,
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let args = Args::from_args();
+    match args.task {
+        None => Args::command().print_help()?,
+        Some(task) => match task {
+            Task::Build => todo!(),
+        },
+    }
+    Ok(())
 }
