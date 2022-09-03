96c5ebdf84f7501776217f94f10ab2cfe50237eb
Quentin Boyer <quentin.boyer@atos.net>
2022-09-03 12:31:05 +0200

    Implement task to build posts

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,7 @@
+use std::path::Path;
+
 use clap::{CommandFactory, Parser};
+use duct::cmd;
 
 #[derive(Parser)]
 struct Args {
@@ -14,11 +17,23 @@ enum Task {
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
+    let manifest = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let workspace = manifest
+        .ancestors()
+        .nth(1)
+        .expect("could not get workspace root");
+
     let args = Args::from_args();
     match args.task {
         None => Args::command().print_help()?,
         Some(task) => match task {
-            Task::Build => todo!(),
+            Task::Build => {
+                let posts = workspace.join("posts");
+                let target = workspace.join("target/html");
+                cmd!(env!("CARGO"), "run", "--", "build", posts, target)
+                    .dir(workspace)
+                    .run()?;
+            }
         },
     }
     Ok(())
