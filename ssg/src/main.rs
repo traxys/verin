a8f878cde3d3837a60602b5a7b8daee26e019651
traxys <quentin+dev@familleboyer.net>
2022-09-05 22:51:33 +0200

    Create templates to handle generic pages

--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -1,15 +1,13 @@
-use std::{
-    fs::OpenOptions,
-    io::{BufWriter, Write},
-    path::PathBuf,
-};
+use std::{collections::HashMap, fs::OpenOptions, io::BufWriter, path::PathBuf};
 
+use chrono::NaiveDate;
 use clap::Parser;
 use color_eyre::{
-    eyre::{Context, ContextCompat},
+    eyre::{self, Context, ContextCompat},
     Result,
 };
 use glob::glob;
+use liquid::Template;
 use serde::Deserialize;
 use ts_highlight_html::{theme, SyntaxConfig};
 
@@ -22,6 +20,7 @@ enum Args {
 struct Metadata {
     date: String,
     title: String,
+    page: String,
 }
 
 mod html;
@@ -43,7 +42,15 @@ fn render_article(
     body: &str,
     output: PathBuf,
     syntax_conf: &SyntaxConfig,
+    templates: &Templates,
 ) -> Result<()> {
+    let template = templates
+        .pages
+        .get(&metadata.page)
+        .ok_or_else(|| eyre::eyre!("Template `{}` does not exist", metadata.page))?;
+
+    let date = NaiveDate::parse_from_str(&metadata.date, "%d/%m/%Y")?;
+
     let mut output = BufWriter::new(
         OpenOptions::new()
             .write(true)
@@ -53,16 +60,25 @@ fn render_article(
             .context("Could not open output file")?,
     );
 
-    writeln!(
-        output,
-        "<!-- date = {}, title = {} -->",
-        metadata.date, metadata.title
-    )?;
+    let mut content = Vec::new();
 
     let body = pulldown_cmark::Parser::new(body);
-    html::write_html(&mut output, body, syntax_conf).context("could not generate html")?;
+    html::write_html(&mut content, body, syntax_conf).context("could not generate html")?;
+
+    template.render_to(
+        &mut output,
+        &liquid::object!({
+            "title": metadata.title,
+            "date": date.format("%d %B %Y").to_string(),
+            "content": String::from_utf8(content).context("generated content was not UTF-8")?,
+        }),
+    )?;
 
     Ok(())
+}
+
+struct Templates {
+    pages: HashMap<String, Template>,
 }
 
 fn main() -> Result<()> {
@@ -70,10 +86,30 @@ fn main() -> Result<()> {
     let args = Args::from_args();
 
     let syntax_conf = SyntaxConfig::new(&*theme::MOONFLY);
+    let mut templates = Templates {
+        pages: HashMap::new(),
+    };
 
     match args {
         Args::Build { input, output } => {
             std::fs::create_dir_all(&output)?;
+
+            for entry in glob(&input.as_path().join("**/*.liquid").to_string_lossy())? {
+                let entry = entry?;
+                let template = liquid::ParserBuilder::with_stdlib()
+                    .build()?
+                    .parse_file(&entry)?;
+
+                templates.pages.insert(
+                    entry
+                        .file_stem()
+                        .expect("Template has no file stem, should not be possible")
+                        .to_str()
+                        .ok_or(eyre::eyre!("Template name should be valid UTF-8"))?
+                        .to_owned(),
+                    template,
+                );
+            }
 
             for entry in glob(&input.as_path().join("**/*.md").to_string_lossy())? {
                 let entry = entry?;
@@ -91,6 +127,7 @@ fn main() -> Result<()> {
                     body,
                     output.join(out).with_extension("html"),
                     &syntax_conf,
+                    &templates,
                 )?;
             }
         }
